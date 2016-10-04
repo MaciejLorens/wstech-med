e6d54aaed162ce78a00fca3595ab4c5b390a3755
Maciej Lorens <maciej.lorens@gmail.com>
2016-10-04 10:32:32 +0200

    fix at_year_at_month scope filtering of order and wz

--- app/models/order.rb
+++ app/models/order.rb
@@ -15,7 +15,7 @@ class Order < ActiveRecord::Base
   scope :at_date, ->(date) { where('created_at >= ? AND created_at < ?', date.beginning_of_day, date.beginning_of_day + 1.day) }
   scope :delivered_at, ->(date) { where('delivery_date >= ? AND delivery_date < ?', date.beginning_of_day, date.beginning_of_day + 1.day) }
   scope :at_status, ->(status) { where('status = ?', status) }
-  scope :at_year_at_month, ->(year, month) { where('created_at >= ? AND created_at < ?', "#{year}/#{month}/01".to_datetime, "#{year}/#{month.to_i + 1}/01".to_datetime) }
+  scope :at_year_at_month, ->(year, month) { where('created_at >= ? AND created_at < ?', "#{year}/#{month}/01".to_datetime, "#{year}/#{month}/01".to_datetime.end_of_month) }
 
   def check_status
     if status == 'not_confirmed' && delivery_request_date.try(:to_date) == confirmation_date.try(:to_date)
