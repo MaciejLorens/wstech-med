eb81372e68563299853969bf1f6b9225c5fd46d4
Maciej Lorens <maciej.lorens@gmail.com>
2018-11-06 12:08:30 +0100

    med init; cleanup

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 maciej = User.create(first_name: 'Maciej', last_name: 'Lorens', email: 'mckl@poczta.fm', password: 'wstechmaciej', admin: true)
 jacek = User.create(first_name: 'Jacek', last_name: 'Mazur', email: 'jmazur@pro.onet.pl ', password: 'wstechjacek', admin: true)
-tomek = User.create(first_name: 'Tomasz', last_name: 'Somer', email: 'tomasz@somer.pl', password: 'wstechtomasz', admin: false)
+
+client = User.create(first_name: 'Tomasz', last_name: 'Somer', email: 'tomasz@somer.pl', password: 'wstechtomasz', admin: false)
 
 
 Order.create(type: 'MetalOrder', user_id: client.id, description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat', quantity: 17, price: 234.77, delivery_request_date: Time.now + 3.days, confirmation_date: Time.now + 4.days)
